bf0cb9cb8a9e1ef09b824317d0fb5df670ca5699
Peltoche <dev@halium.fr>
2019-03-24 12:59:28 +0100

    Move all the ncurses action into the Terminal struct

--- src/devices/terminal.rs
+++ src/devices/terminal.rs
@@ -4,7 +4,39 @@ use std::sync::{Once, ONCE_INIT};
 
 use ncurses::*;
 
+/// The style id is used to override the ncurses default colors and save the
+/// style color. If this number is two hight, some color conflicts will appeares.
+///
+/// Check the `handle_style_change` method documentation for more informations.
+const MAX_STYLE_ID: u32 = 50;
+
 static HANDLER: Once = ONCE_INIT;
+
+pub struct Style {
+    pub color_id: u32,
+    pub italic: bool,
+}
+
+/// An RGB color description.
+///
+/// Each value define the amount of a primary color composing it. The possible
+/// values for each primary color go from `0` for the absence of color to `255`
+/// for the full presence of the color.
+///
+/// Example:
+/// ```rust
+/// let black = RGBColor{r: 0, g: 0, b: 0}
+/// let white = RGBColor{r: 255, g: 255, b: 255}
+/// let red = RGBColor{r: 255, g: 0, b: 0}
+/// ```
+pub struct RGBColor {
+    /// red
+    pub r: u8,
+    /// green
+    pub g: u8,
+    /// blue
+    pub b: u8,
+}
 
 pub struct Terminal {}
 
@@ -22,8 +54,113 @@ impl Terminal {
         Self {}
     }
 
-    pub fn move_cursor(&self, y: usize, x: usize) {
+    pub fn move_cursor(&self, y: u32, x: u32) {
         mv(y as i32, x as i32);
+    }
+
+    pub fn rewrite_line(&self, y: usize) -> LineReWriter {
+        LineReWriter::new(y)
+    }
+
+    /// Return the screen size in term of characters.
+    pub fn get_size(&self) -> (u32, u32) {
+        let mut size_x = 0;
+        let mut size_y = 0;
+        getmaxyx(stdscr(), &mut size_y, &mut size_x);
+
+        (size_y as u32, size_x as u32)
+    }
+
+    pub fn redraw(&mut self) {
+        refresh();
+    }
+
+    /// The style saving process is done via the ncurse routines by overriding
+    /// the existing colors presets via the `init_color` and `init_pair`
+    /// functions. Thoses functionalities are only available for the
+    /// terminals with the `truecolor` capability. In order to check if your
+    /// terminal can handle it check that the output of `echo $COLORTERM` is
+    /// equal to `truecolor` and the output of `echo $TERM` is equal to
+    /// `xterm-256color`.
+    ///
+    /// As the `xterm-256color` feature is set, the terminal preset a set of
+    /// 256 colors with the ids from 0 to 255. As the feature `truecolor` is set
+    /// we can overrid those colors with some arbitrary RGB color. It will save
+    /// the color_pairs (a set of background + foreground color) within the id
+    /// range of [0..50], the foreground colors within the range [50..100] and
+    /// the background colors whithin the range [100...150]. As each style
+    /// correspond to a color pair, which is composed of a background and and
+    /// foreground color it can save only 50 differents styles. After this number
+    /// the colors_pairs saved will override the foreground colors and the
+    /// foreground colors will override the background colors leading to some
+    /// randome colors sets.
+    pub fn save_color_set(&mut self, color_id: u32, fg_color: RGBColor, bg_color: RGBColor) {
+        if color_id > MAX_STYLE_ID {
+            error!(
+                "the new style id is greater than {}, this will load to some randome colors.",
+                MAX_STYLE_ID
+            );
+        }
+
+        // Name space the foreground and background colors.
+        let fg_color_id = 50 + (color_id as i16);
+        let bg_color_id = 100 + (color_id as i16);
+
+        // The `init_color` method take a color range within [0..1000] but the
+        // RGBA colors received by the event are within the range [0..256]. A
+        // rough conversion is done by multiplying the event values by 4.
+        init_color(
+            fg_color_id,
+            i16::from(fg_color.r) * 4,
+            i16::from(fg_color.g) * 4,
+            i16::from(fg_color.b) * 4,
+        );
+
+        init_color(
+            bg_color_id,
+            i16::from(bg_color.r) * 4,
+            i16::from(bg_color.g) * 4,
+            i16::from(bg_color.b) * 4,
+        );
+
+        // Save the new pair of background/foreground color with the `init_pair`
+        // method. The pair_id must be the same id than the style id in order
+        // to avoid translation during the rendering (cf: the
+        // `print_stylized_line` method).
+        init_pair(color_id as i16, fg_color_id, bg_color_id);
+    }
+
+    pub fn set_style(&self, style: &Style) {
+        let attr = COLOR_PAIR(style.color_id as i16);
+        attron(attr);
+
+        if style.italic {
+            attron(A_ITALIC());
+        }
+    }
+
+    pub fn unset_style(&self, style: &Style) {
+        let attr = COLOR_PAIR(style.color_id as i16);
+        attroff(attr);
+
+        if style.italic {
+            attroff(A_ITALIC());
+        }
+    }
+}
+
+pub struct LineReWriter {}
+
+impl LineReWriter {
+    pub fn new(line: usize) -> Self {
+        mv(line as i32, 0);
+        clrtoeol();
+
+        Self {}
+    }
+
+    pub fn push_str(&mut self, s: &str) {
+        addstr(s);
     }
 }
 
--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,17 +1,11 @@
 use std::collections::HashMap;
 
-use crate::devices::terminal::Terminal;
+use crate::devices::terminal::{RGBColor, Style, Terminal};
 
-use ncurses::*;
 use serde_json::Value;
 use xi_rpc::{RemoteError, RpcCall, RpcCtx};
 
-/// The style id is used to override the ncurses default colors and save the
-/// style color. If this number is two hight, some color conflicts will appeares.
-///
-/// Check the `handle_style_change` method documentation for more informations.
-const MAX_STYLE_ID: i16 = 50;
-const SPACES_IN_LINE_SECTION: usize = 2;
+const SPACES_IN_LINE_SECTION: u32 = 2;
 
 #[derive(Eq, PartialEq, Debug)]
 enum RedrawBehavior {
@@ -31,38 +25,12 @@ struct Line {
     is_dirty: bool,
 }
 
-#[derive(Clone)]
-struct Style {
-    id: i16,
-    italic: bool,
-}
-
-impl Style {
-    pub fn set(&self) {
-        let attr = COLOR_PAIR(self.id);
-        attron(attr);
-
-        if self.italic {
-            attron(A_ITALIC());
-        }
-    }
-
-    pub fn unset(&self) {
-        let attr = COLOR_PAIR(self.id);
-        attroff(attr);
-
-        if self.italic {
-            attroff(A_ITALIC());
-        }
-    }
-}
-
 pub struct EventHandler {
     terminal: Terminal,
     /// An index pointing to the Line rendered at the top of the screen.
     ///
     /// Changing its value make the screen scoll up/down.
-    screen_start: usize,
+    screen_start: u32,
 
     styles: HashMap<usize, Style>,
 
@@ -70,12 +38,12 @@ pub struct EventHandler {
     ///
     /// This position take into account the line_section. This means that when
     /// `cursor_x` is equal to 0, its real position is `len(line_section) + 1`.
-    cursor_x: usize,
-    cursor_y: usize,
+    cursor_x: u32,
+    cursor_y: u32,
 
     buffer: Vec<Line>,
     nb_invalid_lines: usize,
-    size_line_section: usize,
+    size_line_section: u32,
 }
 
 impl xi_rpc::Handler for EventHandler {
@@ -95,7 +63,7 @@ impl xi_rpc::Handler for EventHandler {
             _ => debug!("unhandled notif {} -> {}", &rpc.method, &rpc.params),
         };
 
-        refresh();
+        self.terminal.redraw();
     }
 
     fn handle_request(&mut self, _ctx: &RpcCtx, rpc: Self::Request) -> Result<Value, RemoteError> {
@@ -122,30 +90,10 @@ impl EventHandler {
     ///
     /// This function need to create a new set of background/foreground and save
     /// it with the given id.
-    ///
-    /// The style saving process is done via the ncurse routines by overriding
-    /// the existing colors presets via the `init_color` and `init_pair`
-    /// functions. Thoses functionalities are only available for the
-    /// terminals with the `truecolor` capability. In order to check if your
-    /// terminal can handle it check that the output of `echo $COLORTERM` is
-    /// equal to `truecolor` and the output of `echo $TERM` is equal to
-    /// `xterm-256color`.
-    ///
-    /// As the `xterm-256color` feature is set, the terminal preset a set of
-    /// 256 colors with the ids from 0 to 255. As the feature `truecolor` is set
-    /// we can overrid those colors with some arbitrary RGB color. It will save
-    /// the color_pairs (a set of background + foreground color) within the id
-    /// range of [0..50], the foreground colors within the range [50..100] and
-    /// the background colors whithin the range [100...150]. As each style
-    /// correspond to a color pair, which is composed of a background and and
-    /// foreground color it can save only 50 differents styles. After this number
-    /// the colors_pairs saved will override the foreground colors and the
-    /// foreground colors will override the background colors leading to some
-    /// randome colors sets.
     fn handle_style_change(&mut self, body: &Value) {
         #[derive(Deserialize, Debug)]
         struct Event {
-            id: i16,
+            id: u32,
             #[serde(default)]
             italic: bool,
             fg_color: u32,
@@ -155,51 +103,32 @@ impl EventHandler {
 
         let event: Event = serde_json::from_value(body.clone()).unwrap();
 
-        if event.id > MAX_STYLE_ID {
-            error!(
-                "the new style id is greater than {}, this will load to some randome colors.",
-                MAX_STYLE_ID
-            );
-        }
-
-        // Name space the foreground and background colors.
-        let fg_color_id = 50 + event.id;
-        let bg_color_id = 100 + event.id;
-
         // Override the default colors with the `init_color` method. Once save
         // those colors will be accessible via the ids `fg_color_id` and
         // `bg_color_id`.
         //
-        // The `init_color` method take a color range within [0..1000] but the
-        // RGBA colors received by the event are within the range [0..256]. A
-        // rough conversion is done by multiplying the event values by 4.
 
         // fg
         let fg_rgba: [u8; 4] = event.fg_color.to_le_bytes();
-        let fg_r = i16::from(fg_rgba[0]) * 4;
-        let fg_g = i16::from(fg_rgba[1]) * 4;
-        let fg_b = i16::from(fg_rgba[2]) * 4;
-        init_color(bg_color_id, fg_r, fg_g, fg_b);
+        let fg_color = RGBColor {
+            r: fg_rgba[0],
+            g: fg_rgba[1],
+            b: fg_rgba[2],
+        };
 
-        // bg
         let bg_rgba: [u8; 4] = event.bg_color.to_le_bytes();
-        let bg_r = i16::from(bg_rgba[0]) * 4;
-        let bg_g = i16::from(bg_rgba[1]) * 4;
-        let bg_b = i16::from(bg_rgba[2]) * 4;
-        init_color(bg_color_id, bg_r, bg_g, bg_b);
+        let bg_color = RGBColor {
+            r: bg_rgba[0],
+            g: bg_rgba[1],
+            b: bg_rgba[2],
+        };
 
-        // Save the new pair of background/foreground color with the `init_pair`
-        // method. The pair_id must be the same id than the style id in order
-        // to avoid translation during the rendering (cf: the
-        // `print_stylized_line` method).
-
-        // pair
-        init_pair(event.id, fg_color_id, bg_color_id);
+        self.terminal.save_color_set(event.id, fg_color, bg_color);
 
         self.styles.insert(
             event.id as usize,
             Style {
-                id: event.id,
+                color_id: event.id,
                 italic: event.italic,
             },
         );
@@ -214,8 +143,8 @@ impl EventHandler {
         #[derive(Deserialize, Debug)]
         struct Event {
             view_id: String,
-            col: usize,
-            line: usize,
+            col: u32,
+            line: u32,
         }
 
         let event: Event = serde_json::from_value(body.clone()).unwrap();
@@ -223,25 +152,25 @@ impl EventHandler {
         // TODO: Avoid to check the term size at each event by saving it.
         // This will implicate to have some background process checking the
         // window size changes.
-        let size_y = getmaxy(stdscr()) as i32;
+        let (size_y, _) = self.terminal.get_size();
         let mut cursor_y = (event.line as i32) - (self.screen_start as i32);
 
         let mut scroll: bool = false;
-        if cursor_y >= size_y {
+        if cursor_y >= (size_y as i32) {
             // The cursor is bellow the current screen view. Trigger a scroll.
-            self.screen_start += (cursor_y - size_y + 1) as usize;
+            self.screen_start += (cursor_y as u32) - size_y + 1;
             scroll = true;
-            cursor_y -= cursor_y - size_y + 1
+            cursor_y -= cursor_y - (size_y as i32) + 1
         } else if cursor_y <= -1 {
             // The cursor is abor the current screen view. Trigger a scroll.
-            self.screen_start -= (cursor_y).checked_abs().unwrap() as usize;
+            self.screen_start -= cursor_y.checked_abs().unwrap() as u32;
             scroll = true;
             cursor_y = 0;
         }
 
         // Move the cursor at its new position.
         self.cursor_x = event.col;
-        self.cursor_y = cursor_y as usize;
+        self.cursor_y = cursor_y as u32;
 
         if scroll {
             // In case of scroll it need to redraw the screen and after it
@@ -251,7 +180,7 @@ impl EventHandler {
                 &json!({
                 "method": "scroll",
                 "view_id": event.view_id,
-                "params": [self.screen_start , self.screen_start + (size_y as usize) + 1] // + 1 bc range not inclusive
+                "params": [self.screen_start , self.screen_start + size_y  + 1] // + 1 bc range not inclusive
                 }),
             );
 
@@ -364,19 +293,18 @@ impl EventHandler {
         //
         // This size change in function of the number of line du to the size of
         // the number to render. Count the number of spaces set around the section.
-        let new_size_line_section =
-            (new_buffer.len() + self.nb_invalid_lines).to_string().len() + SPACES_IN_LINE_SECTION;
+        let new_size_line_section = ((new_buffer.len() + self.nb_invalid_lines).to_string().len())
+            as u32
+            + SPACES_IN_LINE_SECTION;
         if new_size_line_section != self.size_line_section {
-            let mut size_x = 0;
-            let mut size_y = 0;
-            getmaxyx(stdscr(), &mut size_y, &mut size_x);
+            let (size_y, size_x) = self.terminal.get_size();
             ctx.get_peer().send_rpc_notification(
                 "edit",
                 &json!({
                     "method": "resize",
                     "view_id": event.view_id,
                     "params": {
-                        "width": size_x - (new_size_line_section as i32),
+                        "width": size_x - new_size_line_section ,
                         "height": size_y,
                     }
                 }),
@@ -394,13 +322,13 @@ impl EventHandler {
     /// render it as the top line and fill the screen with all the following
     /// lines.
     fn redraw_view(&mut self, behavior: RedrawBehavior) {
-        let size_y = getmaxy(stdscr()) as usize;
+        let (size_y, _) = self.terminal.get_size();
 
         self.buffer
             .iter()
-            .skip(self.screen_start)
+            .skip(self.screen_start as usize)
             .enumerate()
-            .take_while(|(idx, _)| *idx < size_y)
+            .take_while(|(idx, _)| (*idx as u32) < size_y)
             .for_each(|(idx, line)| {
                 if behavior == RedrawBehavior::Everything || line.is_dirty {
                     self.print_stylized_line(idx, line);
@@ -416,16 +344,14 @@ impl EventHandler {
         let mut screen_x: usize = 0;
         let mut memory: Option<(usize, usize, usize)> = None;
 
-        // Move the the specified line and clear it.
-        self.terminal.move_cursor(screen_y, 0);
-        clrtoeol();
+        let mut line_writer = self.terminal.rewrite_line(screen_y);
 
         // Print the line number.
-        addstr(
+        line_writer.push_str(
             format!(
                 " {:width$} ",
                 line.ln,
-                width = (self.size_line_section - SPACES_IN_LINE_SECTION)
+                width = (self.size_line_section - SPACES_IN_LINE_SECTION) as usize
             )
             .as_str(),
         );
@@ -452,22 +378,22 @@ impl EventHandler {
 
             if style_start == screen_x {
                 let style = &self.styles[&style_id];
-                style.set();
-                addstr(unsafe {
+                self.terminal.set_style(&style);
+                line_writer.push_str(unsafe {
                     line.raw
                         .get_unchecked(style_start..(style_start + style_length))
                 });
-                style.unset();
+                self.terminal.unset_style(&style);
 
                 screen_x += style_length;
             } else {
-                addstr(unsafe { line.raw.get_unchecked(screen_x..style_start) });
+                line_writer.push_str(unsafe { line.raw.get_unchecked(screen_x..style_start) });
 
                 memory = Some((style_start, style_id, style_length));
                 screen_x = style_start;
             }
         }
 
-        addstr(unsafe { line.raw.get_unchecked(screen_x..line_len) });
+        line_writer.push_str(unsafe { line.raw.get_unchecked(screen_x..line_len) });
     }
 }
