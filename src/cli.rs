d5b1c6312f773477bcd43f2826f0e44148531a98
Zhuchii Yeugen <natomist@gmail.com>
2020-01-19 11:43:39 +0200

    More information for cli

--- src/cli.rs
+++ src/cli.rs
@@ -4,5 +4,9 @@ pub fn build() -> App<'static, 'static> {
     App::new("vixi")
         .version(crate_version!())
         .about(crate_description!())
-        .arg(Arg::with_name("file"))
+        .arg(
+            Arg::with_name("file")
+                .help("The file to open")
+                .required(true),
+        )
 }
